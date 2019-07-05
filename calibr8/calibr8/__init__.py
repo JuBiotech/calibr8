03837f4f78324bf44ce97a30c632b8514a3aa164
Helleckes <helleckes@ibt-w2k.kfa-juelich.de>
2019-07-05 10:35:56 +0200

    Adapt __init__.py files for less manual adaptation in the future

--- calibr8/calibr8/__init__.py
+++ calibr8/calibr8/__init__.py
@@ -1,5 +1,5 @@
-from . core import ErrorModel, asymmetric_logistic, inverse_asymmetric_logistic, inverse_logistic, inverse_log_log_logistic, inverse_xlog_logistic, inverse_ylog_logistic, logistic, log_log_logistic, polynomial, xlog_logistic, ylog_logistic
-from . contrib import BiomassErrorModel, BaseGlucoseErrorModel, LinearGlucoseErrorModel, LogisticGlucoseErrorModel
-from . utils import to_did
+from . core import *
+from . contrib import *
+from . utils import *
 
 __version__ = '3.0.0'
