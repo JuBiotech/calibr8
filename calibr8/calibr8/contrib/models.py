21f39d7b8d6bf1c1488f5f6c7081f6caf0e5f40b
Michael Osthege <m.osthege@fz-juelich.de>
2020-03-16 11:13:14 +0100

    mandate that error model constructors must not require (kw)arguments + in load method instantate without args + align contrib models + add tests that check raising of TypeError on when the rule is violated + use dedicated _TestModel types in the tests, because Base models types can no longer be instantiated

--- calibr8/calibr8/contrib/models.py
+++ calibr8/calibr8/contrib/models.py
@@ -5,25 +5,25 @@ from . import base
 
 
 class LinearGlucoseErrorModelV1(base.BasePolynomialModelT):
-    def __init__(self, independent_key, dependent_key, *, scale_degree:int=0, theta_names=None):
-        super().__init__(independent_key=independent_key, dependent_key=dependent_key, mu_degree=1, scale_degree=scale_degree, theta_names=theta_names)
+    def __init__(self, independent_key:str=None, dependent_key:str=None, *, scale_degree:int=0):
+        super().__init__(independent_key=independent_key, dependent_key=dependent_key, mu_degree=1, scale_degree=scale_degree)
         
 
 class LogisticGlucoseErrorModelV1(base.BaseAsymmetricLogisticT):
-    def __init__(self, independent_key:str, dependent_key:str, *, scale_degree:int=0, theta_names=None):
-        super().__init__(independent_key=independent_key, dependent_key=dependent_key, scale_degree=scale_degree, theta_names=theta_names)
+    def __init__(self, independent_key:str=None, dependent_key:str=None, *, scale_degree:int=0):
+        super().__init__(independent_key=independent_key, dependent_key=dependent_key, scale_degree=scale_degree)
 
 
 class CDWBackscatterModelV1(base.BaseAsymmetricLogisticT):
-    def __init__(self, dependent_key:str, independent_key:str='X', *, theta_names=None):
-        super().__init__(independent_key=independent_key, dependent_key=dependent_key, scale_degree=1, theta_names=theta_names)
+    def __init__(self, dependent_key:str='BS', independent_key:str='X'):
+        super().__init__(independent_key=independent_key, dependent_key=dependent_key, scale_degree=1)
 
 
 class CDWAbsorbanceModelV1(base.BaseAsymmetricLogisticT):
-    def __init__(self, independent_key:str='X', dependent_key:str='A600', *, theta_names=None):
-        super().__init__(independent_key=independent_key, dependent_key=dependent_key, scale_degree=1, theta_names=theta_names)
+    def __init__(self, independent_key:str='X', dependent_key:str='A600'):
+        super().__init__(independent_key=independent_key, dependent_key=dependent_key, scale_degree=1)
 
 
 class ODAbsorbanceModelV1(base.BaseAsymmetricLogisticT):
-    def __init__(self, independent_key:str='OD600', dependent_key:str='A600', *, theta_names=None):
-        super().__init__(independent_key=independent_key, dependent_key=dependent_key, scale_degree=1, theta_names=theta_names)
+    def __init__(self, independent_key:str='OD600', dependent_key:str='A600'):
+        super().__init__(independent_key=independent_key, dependent_key=dependent_key, scale_degree=1)
