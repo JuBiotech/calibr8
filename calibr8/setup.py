87e4b9f8a02d2ecfba9568222d5033661822def1
Michael Osthege <m.osthege@fz-juelich.de>
2020-07-28 11:30:44 +0200

    add typing support with py.typed file follows the robotools setup.py as an example

--- calibr8/setup.py
+++ calibr8/setup.py
@@ -1,9 +1,23 @@
 import setuptools
+import os
+import pathlib
+import re
 
 __packagename__ = 'calibr8'
 
+
+def package_files(directory):
+    assert os.path.exists(directory)
+    fp_typed = pathlib.Path(__packagename__, 'py.typed')
+    fp_typed.touch()
+    paths = [str(fp_typed.absolute())]
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
 def get_version():
-    import pathlib, re
     VERSIONFILE = pathlib.Path(pathlib.Path(__file__).parent, __packagename__, 'core.py')
     initfile_lines = open(VERSIONFILE, 'rt').readlines()
     VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
@@ -17,9 +31,9 @@ __version__ = get_version()
 
 
 setuptools.setup(name = __packagename__,
-        packages = setuptools.find_packages(), # this must be the same as the name above
+        packages = setuptools.find_packages(),
         version=__version__,
-        description='Package for biological error models.',
+        description='Toolbox for non-linear calibration and error modeling.',
         url='https://jugit.fz-juelich.de/ibg-1/micropro/calibr8',
         author='Laura Marie Helleckes, Michael Osthege',
         author_email='l.helleckes@fz-juelich.de, m.osthege@fz-juelich.de',
@@ -27,13 +41,13 @@ setuptools.setup(name = __packagename__,
         classifiers= [
             'Programming Language :: Python',
             'Operating System :: OS Independent',
-            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
             'Intended Audience :: Developers'
         ],
         install_requires=[
             'numpy',
             'scipy',
             'matplotlib',
-            'fastprogress'
+            'fastprogress',
         ]
 )
