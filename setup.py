d51b429e15e862a7678b26f25d63fcba991c4cf7
Michael Osthege <m.osthege@fz-juelich.de>
2020-10-24 16:20:00 +0200

    set license & update setup.py

--- setup.py
+++ setup.py
@@ -30,28 +30,30 @@ def get_version():
 __version__ = get_version()
 
 
-setuptools.setup(name = __packagename__,
-        packages = setuptools.find_packages(),
-        version=__version__,
-        description='Toolbox for non-linear calibration and error modeling.',
-        url='https://jugit.fz-juelich.de/ibg-1/micropro/calibr8',
-        author='Laura Marie Helleckes, Michael Osthege',
-        author_email='l.helleckes@fz-juelich.de, m.osthege@fz-juelich.de',
-        license='(c) 2020 Forschungszentrum Jülich GmbH',
-        classifiers= [
-            'Programming Language :: Python',
-            'Operating System :: OS Independent',
-            'Programming Language :: Python :: 3.7',
-            'Intended Audience :: Developers'
-        ],
-        install_requires=[
-            'numpy',
-            'scipy',
-            'matplotlib',
-            'fastprogress',
-        ],
-        package_data={
-            'calibr8': package_files(str(pathlib.Path(pathlib.Path(__file__).parent, 'calibr8').absolute()))
-        },
-        include_package_data=True
+setuptools.setup(
+    name=__packagename__,
+    packages=setuptools.find_packages(),
+    version=__version__,
+    description='Toolbox for non-linear calibration and error modeling.',
+    long_description=open(pathlib.Path(ROOT, 'README.md')).read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/michaelosthege/calibr8',
+    author='Laura Marie Helleckes, Michael Osthege',
+    author_email='l.helleckes@fz-juelich.de, m.osthege@fz-juelich.de',
+    license='AGPLv3',
+    classifiers= [
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ],
+    install_requires=[
+        open(pathlib.Path(ROOT, 'requirements.txt')).readlines()
+    ],
+    package_data={
+        'calibr8': package_files(str(pathlib.Path(pathlib.Path(__file__).parent, 'calibr8').absolute()))
+    },
+    include_package_data=True,
+    python_requires='>=3.6'
 )
